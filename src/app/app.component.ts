c2b366dc75f1cf9b7fbddda876d9847cb88d57d9
jair <edgarjairmartinezruiz000@gmail.com>
2025-04-15 17:16:13 -0600

    Agregando componente tarea-panel con botones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './index.html',
+  styleUrls: ['./styles.css']
 })
 export class AppComponent {
-  value = 'World';
+  subir() {
+    console.log('Subir tarea');
+    // lógica para subir
+  }
+
+  eliminar() {
+    console.log('Eliminar tarea');
+    // lógica para eliminar
+  }
+
+  marcarUrgente() {
+    console.log('Tarea marcada como urgente');
+    // lógica para marcar como urgente
+  }
 }
+
+
